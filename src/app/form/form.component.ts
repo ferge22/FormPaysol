1e34465ee56899ec00e457b9990b7b68b8f598a6
Rytis Spitrys <rytis.spitrys@visma.com>
2021-05-11 18:31:30 +0300

    trigger alert after form is submited

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -12,6 +12,11 @@ export class FormComponent implements OnInit, OnDestroy {
   form: FormGroup;
   private readonly subscriptions = new Subscription();
 
+  // tslint:disable-next-line: max-line-length
+  // just created simplee component for aler, could have done like alert service but it would taken more time to impement, so its just simple solution for this task
+  showAlert: boolean;
+  alertMessage: string;
+
   constructor(
     private formBuilder: FormBuilder,
     private contactService: ContactService
@@ -29,11 +34,22 @@ export class FormComponent implements OnInit, OnDestroy {
   }
 
   submit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
     this.subscriptions.add(
-      this.contactService.send({ ...this.form.value }).subscribe(
-        () => {
-          
-        })
+      this.contactService.send({ ...this.form.value }).subscribe(() => {
+        this.showAlert = true;
+        this.alertMessage = 'Form submited!';
+        this.form.reset();
+
+        // hide alert after 1.5s
+        setTimeout(() => {
+          this.showAlert = false;
+        }, 1500);
+      })
     );
   }
 
