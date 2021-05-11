a40d7564ab7bad3859921498f0858daae7afb75e
Rytis Spitrys <rytis.spitrys@visma.com>
2021-05-11 18:15:03 +0300

    formGroup init with template(bootstrap styles)

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  private initForm(): void {
+    this.form = this.formBuilder.group({
+      email: [null],
+      name: [null],
+    });
+  }
+
+  submit(): void {}
 }
