5937eadedafbfd8ccac6f6b45b33c8f8958cc489
Rytis Spitrys <rytis.spitrys@visma.com>
2021-05-11 18:58:08 +0300

    finishing touches, routing redirect if path not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,15 @@ const routes: Routes = [
   {
     path: '',
     component: FormComponent,
-  }
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
